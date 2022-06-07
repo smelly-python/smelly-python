bd624da004177ebd6c23d8a79e8c61730a5f2084
Wouter Polet <wouterpolet@gmail.com>
2022-06-07 17:19:28 +0200

    Fix code style errors

--- smelly_python/code_smell.py
+++ smelly_python/code_smell.py
@@ -1,28 +1,56 @@
+"""
+The code smell modules provides the CodeSmell class
+and the Location class, which is used by the CodeSmell.
+"""
 from multiprocessing.dummy import Array
 
 
-class Code_Smell(object): 
+class Location:
+    """
+    The Location class contains the location information of a CodeSmell.
+    This includes the module, object, line and column numbers of the code smell
+    in the file that the path points to.
+    """
     def __init__(self, data):
-        self.type = data['type']
         self.module = data['module']
-        self.obj = data['obj']
+        self.python_object = data['obj']
         self.line = data['line']
         self.column = data['column']
-        self.endLine = data['endLine']
+        self.end_line = data['end_line']
         self.path = data['path']
+
+    def __repr__(self) -> str:
+        return f'in {self.module} on line {self.line} at {self.column}'
+
+    def __str__(self):
+        return f'in {self.module} on line {self.line} at {self.column}'
+
+
+class CodeSmell:
+    """
+    The CodeSmell class contains all the fields of the JSON objects that pylint generates.
+    """
+    def __init__(self, data):
+        self.type = data['type']
+        self.location = Location(data)
         self.symbol = data['symbol']
         self.message = data['message']
         self.message_id = data['message-id']
 
     def __repr__(self) -> str:
-        return f"{self.type} in {self.module} on line {self.line} at {self.column} with reason: {self.message}"
+        return f'{self.type} {repr(self.location)} with reason: {self.message}'
 
     def __str__(self) -> str:
-        return f"{self.type} in {self.module} on line {self.line} at {self.column} with reason: {self.message}"
+        return f'{self.type} {self.location} with reason: {self.message}'
 
     @staticmethod
     def convert_dict(json_content) -> Array:
+        """
+        Converts
+        :param json_content:
+        :return:
+        """
         ret = []
         for i in json_content:
-            ret.append(Code_Smell(i))
+            ret.append(CodeSmell(i))
         return ret
--- smelly_python/generator/webpage_generator.py
+++ smelly_python/generator/webpage_generator.py
@@ -1,12 +1,21 @@
-from dominate import document
-from dominate.tags import *
+"""
+The webpage generator module provides the method that generates the webpage given a list of
+style errors.
+"""
 from os import path
+from dominate import document
+from dominate.tags import h1, style
 
 
 def generate_webpage():
+    """
+    Generates the webpage showing the errors as a string.
+    :return: the html webpage as a string
+    """
     doc = document(title='Smelly Python code smell report')
 
-    with open(path.join(path.dirname(__file__), '../resources/style.css'), 'r', encoding='utf-8') as style_file:
+    with open(path.join(path.dirname(__file__), '../resources/style.css'), 'r', encoding='utf-8')\
+            as style_file:
         with doc.head:
             style(style_file.read())
 
