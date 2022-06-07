2dee3a8129159f92529da216f508b87131950eea
Cedric Willekens <c.s.willekens@student.tudelft.nl>
2022-06-07 15:06:06 +0200

    Add reading of the json file

--- smelly_python/command_line.py
+++ smelly_python/command_line.py
@@ -1,2 +1,12 @@
-def main(): 
-    print("hello world")
+import sys
+import json
+
+def main():
+    print(f"Argument count: {len(sys.argv)}")
+    if len(sys.argv) < 2: 
+        print('Insufficient parameters provided')
+        sys.exit(1)
+    with open(sys.argv[1]) as file: 
+        dictData = json.load(file)
+        print(dictData)
+    
